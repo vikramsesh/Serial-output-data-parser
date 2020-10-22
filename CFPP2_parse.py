afe5f5f4a4fb991bf9f5324d693f3cc288969875
Vikram Seshadri <34171350+vikramsesh@users.noreply.github.com>
2020-10-22 16:04:45 -0400

    Add files via upload

---
+++ CFPP2_parse.py
@@ -0,0 +1,101 @@
+import os
+import sys
+import xlsxwriter
+import re
+
+filename = r"C:\Users\vseshadri\Downloads\CFP Serial data\CFP300 #6\CFP300 6#_32th-51th cycle _very hot water_50oz.CSV"
+f = open(filename,"r")
+data = f.read()
+
+# Create an new Excel file and add a worksheet.
+workbook = xlsxwriter.Workbook(r'C:\Users\vseshadri\Downloads\CFP Serial data\CFP300 #6\CFP300 6#_32th-51th cycle _very hot water_50oz.xlsx')
+worksheet = workbook.add_worksheet('Data')
+worksheet.freeze_panes(1, 0)
+
+##entries = [i for i in data.split('\n')]
+entries = data.split('\n')
+entries.remove(entries[0])
+
+##for i in range(0, len(entries)-1):
+##    print(entries[i])
+    
+HEADERS = ['Time','Outlet Temp', 'Boiler Temp', 'Warm Plate Temp', 'Max Temp', 'Calibrated Offset Temp', 'Pump PWM', 'Boiler On/Off', 'PTC On/Off',
+           'Flow rate', 'Current Block Volume', 'Current Total Volume', 'Clean Count', 'Recipe Size', 'Recipe Brew',
+           'Recipe Block', 'Recipe Total Volume', 'Recipe Time']
+     
+SUB_HEADERS = ['sec.','degC','degC','degC','degC','degC']
+
+
+##worksheet2 = workbook.add_worksheet()
+
+# Add a bold format to use to highlight cells.
+header_format  = workbook.add_format()
+header_format.set_align('center')
+header_format.set_bold()
+header_format.set_text_wrap()
+header_format.set_align('vcenter')
+
+subheader_format  = workbook.add_format()
+subheader_format.set_align('center')
+subheader_format.set_bold()
+subheader_format.set_italic()
+subheader_format.set_text_wrap()
+subheader_format.set_align('vcenter')
+
+data_format  = workbook.add_format()
+data_format.set_align('vcenter')
+data_format.set_align('center')
+
+row_index = 0
+
+# Write headers
+for index, i in enumerate(HEADERS):
+    worksheet.write(row_index, index, i, header_format)
+for index, i in enumerate(SUB_HEADERS):
+    worksheet.write(row_index + 1, index, i, subheader_format)
+
+row_index = 1
+
+try:
+    for i in range(0, len(entries)):
+        row_index += 1
+        data = entries[i].split(' ')
+        
+        worksheet.write(row_index,0, i+1 ,data_format) 
+        for k in range(0, len(data)): 
+            try:
+                data[k] = float(data[k])
+                if k<=5:
+                    data[k] = float(data[k])/10
+                  
+                worksheet.write(row_index,k+1, data[k],data_format)
+            except:
+                txt = data[k]
+                x = re.search(r"(B)(.)", txt)
+                y = re.search(r"(P)(.)", txt)
+                if x:
+                    data[k] = x.group(2)
+                    data[k] = float(data[k])
+
+                if y:
+                    data[k] = y.group(2)
+                    data[k] = float(data[k])
+                    
+                worksheet.write(row_index, k+1, data[k],data_format)
+                pass
+        print(data)
+except:
+    pass
+    
+while True:
+    try:
+        workbook.close()
+    except xlsxwriter.exceptions.FileCreateError as e:
+        # For Python 3 use input() instead of raw_input().
+        decision = raw_input("Exception caught in workbook.close(): %s\n"
+                             "Please close the file if it is open in Excel.\n"
+                             "Try to write file again? [Y/n]: " % e)
+        if decision != 'n':
+            continue
+
+    break
---
+++ OL600BDP.py
@@ -0,0 +1,79 @@
+import xlsxwriter
+import pandas as pd
+import re
+import os
+import sys
+import openpyxl
+
+HEADERS = ['HeatSink','AF NTC','PC NTC','Probe1 NTC',
+           'Probe2 NTC', 'High Pressure','Low Pressure','SW version (Release.Version.Revision)','Build date']
+
+path = "C:\\Users\\vseshadri\\Desktop\\"
+filename = "Chilli2_126V_Serial.txt"
+file = path + filename
+
+f = open(file)
+data = f.read()
+
+pattern = r'(\?KN1\n)'
+pattern1 = r'(\n\?KN2\n)'
+pattern2 = r'(\n\?KN3\n)'
+pattern3 = r'(\n\?KN4\n)'
+pattern4 = r'(\n\?KN5\n)'
+pattern5 = r'(\n\?SW2\n)'
+pattern6 = r'(\n\?SW1\n)'
+pattern7 = r'(\n\?WZ\n)'
+
+try:
+    data = re.sub(pattern, '', data)
+    data = re.sub(pattern1, ',', data)
+    data = re.sub(pattern2, ',', data)
+    data = re.sub(pattern3, ',', data)
+    data = re.sub(pattern4, ',', data)
+    data = re.sub(pattern5, ',', data)
+    data = re.sub(pattern6, ',', data)
+    data = re.sub(pattern7, ',', data)
+except:
+    pass
+
+data = data.split('\n')
+data.remove(data[0])
+data.remove(data[len(data)-1])
+data.remove(data[len(data)-1])
+
+workbook = xlsxwriter.Workbook("Chilli2_126V_Serial.xlsx")
+worksheet1 = workbook.add_worksheet('Data')
+##worksheet2 = workbook.add_worksheet('RAW')
+
+header_format = workbook.add_format()
+header_format.set_align('center')
+header_format.set_bold()
+header_format.set_text_wrap()
+header_format.set_align('vcenter')
+
+cell_format = workbook.add_format()
+cell_format.set_align('center')
+##cell_format.set_text_wrap()
+
+for index, i in enumerate(HEADERS):
+    worksheet1.write(0, index, i,header_format)
+                 
+for row in range (0,len(data)):
+    
+    cell = data[row].split(',')
+    for col in range (0,len(cell)):
+        worksheet1.write(row+1, col+len(HEADERS)+1, cell[col],cell_format)
+        if col<5:
+            convert_cell = int(cell[col][5:],16)/10
+        elif col>=5 and col<7:
+            convert_cell = int(cell[col][4:])
+        x = re.match(r'(\$WZ)(..)(..)(..)(\w+........)',cell[col])
+        if x:
+            convert_cell = x.group(2)+'.'+ x.group(3)+'.'+x.group(4)
+            worksheet1.write(row+1, col+1, x.group(5),cell_format)
+        else:
+            pass
+        
+        worksheet1.write(row+1, col, convert_cell,cell_format)
+ 
+workbook.close()
